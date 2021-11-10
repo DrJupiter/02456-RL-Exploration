451ed0109e6c93e3f4507e1b3e74fbb236efc1ad
DrJupiter <klausjupiter@hotmail.com>
2021-11-10 14:55:34 +0100

    10

---
+++ ppo.py
@@ -0,0 +1,127 @@
+import torch
+from torch import tensor
+from torch import nn
+import torch.nn.functional as F
+from torch import optim
+
+# act_probs from softmax on output from policy net
+# as in the paper
+def sample_action(act_probs: tensor) -> int:
+    return torch.multinomial(act_probs,num_samples=1).squeeze()
+
+# Baseline policy net
+class PolicyNet(nn.Module):
+    """Policy network"""
+
+    def __init__(self, n_inputs, n_hidden, n_outputs, learning_rate):
+        super(PolicyNet, self).__init__()
+        # network
+        self.hidden = nn.Linear(n_inputs, n_hidden)
+        self.out = nn.Linear(n_hidden, n_outputs)
+        # training
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+
+    def forward(self, x):
+        x = self.hidden(x)
+        x = F.relu(x)
+        x = self.out(x)
+        return F.softmax(x, dim=1)
+
+    # I think this loss requires some modifications 
+    def loss(self, action_probabilities, returns):
+        return -torch.mean(torch.mul(torch.log(action_probabilities), returns))
+
+
+
+from torch.nn import init
+import numpy as np
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+"""
+In RND there are 2 networks:
+- Target Network: generates a constant output for a given state
+- Prediction network: tries to predict the target network's output
+""" 
+class RNDModel(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(RNDModel, self).__init__()
+
+        self.input_size = input_size
+        self.output_size = output_size
+
+        feature_output = 7 * 7 * 64
+
+        # Prediction network
+        self.predictor = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=32,
+                kernel_size=8,
+                stride=4),
+            nn.ELU(),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=4,
+                stride=2),
+            nn.ELU(),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=3,
+                stride=1),
+            nn.ELU(),
+            Flatten(),
+            nn.Linear(feature_output, 512),
+            nn.ELU(),
+            nn.Linear(512, 512),
+            nn.ELU(),
+            nn.Linear(512, 512)
+        )
+
+        # Target network
+        self.target = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=32,
+                kernel_size=8,
+                stride=4),
+            nn.ELU(),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=4,
+                stride=2),
+            nn.ELU(),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=3,
+                stride=1),
+            nn.ELU(),
+            Flatten(),
+            nn.Linear(feature_output, 512)
+        )
+
+        # Initialize the weights and biases
+        for p in self.modules():
+            if isinstance(p, nn.Conv2d):
+                init.orthogonal_(p.weight, np.sqrt(2))
+                p.bias.data.zero_()
+
+            if isinstance(p, nn.Linear):
+                init.orthogonal_(p.weight, np.sqrt(2))
+                p.bias.data.zero_()
+
+        # Set that target network is not trainable
+        for param in self.target.parameters():
+            param.requires_grad = False
+
+    def forward(self, next_obs):
+        target_feature = self.target(next_obs)
+        predict_feature = self.predictor(next_obs)
+
+        return predict_feature, target_feature
+
