0459cbf8955911eb1f60e31e2ebd64c9a561d364
DrJupiter <klausjupiter@hotmail.com>
2021-11-11 20:24:35 +0100

    rnd adn gym

--- monz_gym.py
+++ monz_gym.py
@@ -5,4 +5,7 @@ env.reset()
 for _ in range(1000):
     env.render()
     env.step(env.action_space.sample()) # take a random action
+
+    if done:
+        observation = env.reset()
 env.close()
--- rnd.py
+++ rnd.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from torch.nn import init
 import numpy as np
 from torch import nn
@@ -10,19 +11,40 @@ In RND there are 2 networks:
 - Target Network: generates a constant output for a given state
 - Prediction network: tries to predict the target network's output
 """ 
+
+
+from math import floor
+
+def cnn_dims(data_size, kernel_size, stride, padding, filters):
+    """
+    -> out 
+
+    feature out can be calculated by product(out[4:7])
+    """
+    _batch, in_channel, height, width = data_size
+    out_height = floor((height + 2 * padding[1] - (kernel_size[1]-1)-1)
+                             /stride[1] + 1)
+    out_width= floor((width + 2 * padding[0] - (kernel_size[0]-1)-1)
+                             /stride[0] + 1)
+    return [in_channel,kernel_size,stride,padding,filters, out_height,out_width]
+
+
+
+#18 (210, 160, 3)
+
 class RNDModel(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size: Tuple, output_size: int):
         super(RNDModel, self).__init__()
 
-        self.input_size = input_size
-        self.output_size = output_size
+        self.input_size = input_size # WxHxC reshaped into CxHxW. Default 210x160x3
+        self.output_size = output_size 
 
-        feature_output = 7 * 7 * 64
+        feature_output = 2**11 * 11 # the symmetry # still very cursed
 
         # Prediction network
         self.predictor = nn.Sequential(
             nn.Conv2d(
-                in_channels=1,
+                in_channels=input_size[2], # something
                 out_channels=32,
                 kernel_size=8,
                 stride=4),
@@ -44,13 +66,14 @@ class RNDModel(nn.Module):
             nn.ELU(),
             nn.Linear(512, 512),
             nn.ELU(),
-            nn.Linear(512, 512)
+            nn.Linear(512, output_size)
         )
 
-        # Target network
+       
+        # Target network (Never updated)
         self.target = nn.Sequential(
             nn.Conv2d(
-                in_channels=1,
+                in_channels=input_size[2],
                 out_channels=32,
                 kernel_size=8,
                 stride=4),
@@ -68,7 +91,11 @@ class RNDModel(nn.Module):
                 stride=1),
             nn.ELU(),
             Flatten(),
-            nn.Linear(feature_output, 512)
+            nn.Linear(feature_output, 512),
+            nn.ELU(),
+            nn.Linear(512, 512),
+            nn.ELU(),
+            nn.Linear(512, output_size)
         )
 
         # Initialize the weights and biases
